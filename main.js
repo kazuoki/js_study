973376e23b30256d5c82da6307bffcc587164c67
Kazuki Okimori <kz.ok.0116@gamil.com>
2020-09-23 20:54:58 +0900

    数値の計算

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 // document.getElementById('box').textContent='こんばんは';
 // document.getElementById('box').textContent=new Date().toLocaleString();
-document.getElementById('box').textContent='abc'+'def';
+// document.getElementById('box').textContent='abc'+'def';
+document.getElementById('box').textContent = 1+ 1;

f2bb9149a3b9265b347ebdc1e0b4f8769127bf19
Kazuki Okimori <kz.ok.0116@gamil.com>
2020-09-23 21:04:55 +0900

    クリックイベントの呼び出し

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 // document.getElementById('box').textContent='こんばんは';
 // document.getElementById('box').textContent=new Date().toLocaleString();
 // document.getElementById('box').textContent='abc'+'def';
-document.getElementById('box').textContent = 1+ 1;
+// document.getElementById('box').textContent = 1+ 1;
+const button = document.getElementById('my-button');
+button.addEventListener('click', () => {
+
+});

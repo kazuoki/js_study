546cd569928a70d780ddefe3292d11149dbceb4c
Kazuki Okimori <kz.ok.0116@gamil.com>
2020-09-23 20:50:39 +0900

    文字同士の結合（join）

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 // document.getElementById('box').textContent='こんばんは';
-document.getElementById('box').textContent=new Date().toLocaleString();
+// document.getElementById('box').textContent=new Date().toLocaleString();
+document.getElementById('box').textContent='abc'+'def';

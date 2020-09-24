5562b88e352502d7ca92ae3e926904c3b2060524
Kazuki Okimori <kz.ok.0116@gamil.com>
2020-09-25 02:01:17 +0900

    セレクトボックスの追加 フォームの追加

--- main.js
+++ main.js
@@ -13,29 +13,35 @@ button.addEventListener('click', () => {
   alert('押された！');
 });
 
+
+// id"box"取得し変数boxへ代入
 const box = document.getElementById('box');
 
-// マウスが乗った時
-// box.addEventListener(
-//   'mouseenter',
-//   // e.targetはイベント発生元の要素（<div id="box">）
-//   (e) => {
-//     e.target.textContent = 'マウスが乗った！';
-//   }
-// );
-
-
-// マウスが離れた時
-// box.addEventListener(
-//   'mouseleave',
-//   (e) => {
-//   e.target.textContent = 'マウスが離れた！';
-//   }
-// );
+// マウスが乗った時のテキスト
 box.addEventListener('mouseenter',(e) => {
   e.target.textContent=('つかれたよん');
 })
 
+
+// マウス離れた時のテキスト
 box.addEventListener('mouseleave',(e) => {
   e.target.textContent=('お疲れ様');
 })
+
+
+// セレクトの切り替え
+const select = document.getElementById('my-select');
+
+select.addEventListener('change',(e) => {
+  console.log('value:',e.target.value);
+});
+
+
+// フォームの送信
+const input = document.getElementById('input-word');
+const form = document.getElementById('my-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log('value: ', input.value);
+});

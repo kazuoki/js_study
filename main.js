0676df2444d92a4609ee203cd5dff1f27e0c0dbc
Kazuki Okimori <kz.ok.0116@gamil.com>
2020-09-24 23:15:54 +0900

    クリックイベントのスニペット追加

--- main.js
+++ main.js
@@ -2,7 +2,13 @@
 // document.getElementById('box').textContent=new Date().toLocaleString();
 // document.getElementById('box').textContent='abc'+'def';
 // document.getElementById('box').textContent = 1+ 1;
-const button = document.getElementById('my-button');
-button.addEventListener('click', () => {
+// const button = document.getElementById('my-button');
+// button.addEventListener('click', () => {
 
+// });
+
+const button = document.getElementById('my-button');
+
+button.addEventListener('click', () => {
+  alert('押された！');
 });

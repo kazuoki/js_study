8c06340c08a85156221e21db2ebf7c2e690af4b6
Kazuki Okimori <kz.ok.0116@gamil.com>
2020-09-24 23:52:57 +0900

    マウスエンター

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// document.getElementById('box').textContent='こんばんは';
+document.getElementById('box').textContent='こんばんは';
 // document.getElementById('box').textContent=new Date().toLocaleString();
 // document.getElementById('box').textContent='abc'+'def';
 // document.getElementById('box').textContent = 1+ 1;
@@ -12,3 +12,14 @@ const button = document.getElementById('my-button');
 button.addEventListener('click', () => {
   alert('押された！');
 });
+
+const box = document.getElementById('box');
+
+// マウスが乗った時
+box.addEventListener(
+  'mouseenter',
+  // e.targetはイベント発生元の要素（<div id="box">）
+  (e) => {
+    e.target.textContent = 'マウスが乗った！';
+  }
+);
